9b3f79cab2617959bf1036d07f726f31b2a91139
sgk <3228891558@qq.com>
2024-06-05 16:08:11 +0800

    chore: modify some files

--- main.go
+++ main.go
@@ -2,114 +2,58 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"unicode/utf8"
 )
 
-func convert(value, scale int) string {
-	switch scale {
-	case 2:
-		return fmt.Sprintf("0b%b", value)
-	case 8:
-		return fmt.Sprintf("0o%o", value)
-	case 16:
-		return fmt.Sprintf("0x%x", value)
+func Str(value interface{}) string {
+	switch value.(type) {
+	case int:
+		return fmt.Sprintf("%d", value)
+	case bool:
+		return fmt.Sprintf("%t", value)
+	case float64:
+		return fmt.Sprintf("%f", value)
 	default:
-		panic("invalidate scale!")
+		return fmt.Sprintf("%v", value)
 	}
 }
 
-func upperCase(value string) string {
-	codes := []rune(value)
-
-	for idx, code := range codes {
-		if code >= 97 && code <= 122 {
-			codes[idx] -= 32
-		}
-	}
-
-	return string(codes)
-}
-
-func Input() {
-	fmt.Scanln()
-}
-
-func hasKey(m map[string]interface{}, key string) bool {
-	if _, exist := m[key]; exist {
-		return true
+func ParseInt(s string) (int, error) {
+	if res, err := strconv.Atoi(s); err == nil {
+		return res, nil
 	} else {
-		return false
+		return 0, fmt.Errorf("")
 	}
 }
-
-type number = float64
 
 func main() {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(rand.Float64(), Str(78.12), Str(false))
 
-	var (
-		username string = "Randall Lloyd"
-		email    string = "hi@vu.nf"
-	)
-
-	fmt.Printf("<%s, %s>\n", username, email)
-	fmt.Println(Author, convert(12, 2))
-	fmt.Printf("%T %U %s %p %v\n", '😃', '🙂', upperCase("hi, golang!"), new(int), *new(float64))
-	fmt.Printf("%5.2f %s\n", 3.1415926, `\n\t\r`)
-
-	// var rate float64
-
-	// if _, err := fmt.Scanln(&rate); err == nil {
-	// 	fmt.Printf("%v %T\n", rate, rate)
-	// }
-
-	fmt.Println(func(x, y int) (max int) {
-		if x > y {
-			max = x
-		} else {
-			max = y
-		}
-		return
-	}(67, 99))
-
-	players := [...]string{"Robert Sanchez", "Martin Powell", "Lina Blake"}
-
-	points := [5][2]float64{
-		{39.67, 38.04},
-		{95.63, 40.30},
-		{41.57, 12.54},
+	if pixel, err := ParseInt("12.12"); err == nil {
+		fmt.Printf("%v %T\n", pixel, pixel)
 	}
 
-	whiteList := []string{5: ""}
+	searchParams := "q=flutter&form=QBLH&sp=-1&lq=0&pq=flu&sc=10-3&qs=n"
 
-	whiteList = append(whiteList, []string{"", ""}...)
+	fmt.Println(strings.Contains("", ""), strings.ReplaceAll("", "", ""))
+	fmt.Println(strings.Fields(""), strings.Split(searchParams, "&"), strings.Trim("", ""))
 
-	fmt.Printf("%T %v %d %d\n", players, points, len(whiteList), cap(whiteList))
+	greet := "你好，世界！"
 
-	for idx, char := range "极限挑战" {
-		fmt.Printf("%d => %c\n", idx, char)
+	fmt.Println(utf8.RuneCountInString(greet), utf8.ValidString(""))
+
+	if duration, err := time.ParseDuration(""); err == nil {
+		fmt.Println(duration.String())
 	}
 
-	config := make(map[string]interface{})
+	fileInfo, err := os.Stat("")
 
-	config["name"] = ""
-	config["license"] = ""
-	config["version"] = ""
+	fmt.Println(fileInfo.IsDir(), os.IsExist(err))
 
-	delete(config, "name")
-
-	fmt.Println(config, hasKey(config, "name"), hasKey(config, "author"))
-
-	var port number = 9527
-
-	fmt.Printf("%T\n", port)
-
-}
-
-func init() {
-	fmt.Println("main::init called ... ")
 }
