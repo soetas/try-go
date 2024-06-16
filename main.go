f787fc3df52a70a6e29536dd36cebbf348256fe3
soetas <3228891558@qq.com>
2024-06-16 17:43:36 +0800

    chore: modify some files

--- main.go
+++ main.go
@@ -1,38 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
-type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Phone string `json:"phone,omitempty"`
-}
-
-func typeof(value interface{}) string {
-	return fmt.Sprintf("%v", reflect.TypeOf(value).Kind())
-}
-
-func view(value interface{}) {
-	if reflect.TypeOf(value).Kind() == reflect.Struct {
-		val := reflect.ValueOf(value)
-		for i := 0; i < val.NumField(); i++ {
-			fmt.Printf("%v\n", val.Field(i))
-		}
-	}
-}
-
 func main() {
-	fmt.Println(typeof(1i), typeof(User{}))
 
-	view(User{
-		Name:  "Brett Curtis",
-		Email: "wi@hov.in",
-	})
-
-	s := new([]int)
-
-	fmt.Printf("%#v %t\n", s, *s == nil)
 }
