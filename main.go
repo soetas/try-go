25783e8ebfd0d4449aad45d431b461c4dce7297f
soetas <3228891558@qq.com>
2024-06-16 10:57:17 +0800

    chore: modify some files

--- main.go
+++ main.go
@@ -1,130 +1,35 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"math"
 	"reflect"
-	"unsafe"
 )
 
-type any = interface{}
-
-type object map[string]interface{}
-
-func (o object) toJson() string {
-	res, _ := json.Marshal(o)
-	return string(res)
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone,omitempty"`
 }
 
 func typeof(value interface{}) string {
-	return fmt.Sprintf("%T", value)
+	return fmt.Sprintf("%v", reflect.TypeOf(value).Kind())
 }
 
-func has(m map[string]interface{}, k string) bool {
-	if _, ok := m[k]; ok {
-		return true
-	} else {
-		return false
+func view(value interface{}) {
+	if reflect.TypeOf(value).Kind() == reflect.Struct {
+		val := reflect.ValueOf(value)
+		for i := 0; i < val.NumField(); i++ {
+			fmt.Printf("%v\n", val.Field(i))
+		}
 	}
 }
-
-func log(values ...interface{}) {
-	fmt.Println(values...)
-}
-
-const PI = math.Pi
-
-const (
-	UNSENT = iota
-	OPENED
-	LOADING
-	DONE
-)
-
-type HTTPServer struct {
-	Hostname string
-	Port     int
-	Open     bool
-}
-
-var (
-	ErrType = errors.New("")
-)
 
 func main() {
-	defer func() {
-		if res := recover(); res != nil {
-			if err, ok := res.(error); ok {
-				fmt.Println(err.Error())
-			}
-		}
-	}()
+	fmt.Println(typeof(1i), typeof(User{}))
 
-	guid := "ba730025-9b81-57c4-b5d1-d765e7bb4c75"
+	view(User{
+		Name:  "Brett Curtis",
+		Email: "wi@hov.in",
+	})
 
-	var (
-		username string = "Dylan Gonzales"
-		email    string = "fe@vek.gi"
-	)
-
-	fmt.Println(typeof(0b101), typeof((guid)), fmt.Sprintf("<%s, %s>", username, email))
-	fmt.Println(unsafe.Sizeof(0), math.MaxInt8)
-	fmt.Println(typeof(.5))
-
-	cities := [...]string{3: "TJ", 5: "SH"}
-	coords := [...][2]float64{
-		{69.11, 34.28},
-		{19.49, 41.18},
-		4: {-170.43, -14.16},
-	}
-
-	fmt.Println(cities, typeof(cities), typeof(coords))
-
-	players := []string{"Cornelia Greer", 2: "Tony Greer", "Wayne Long"}
-
-	players = append(players, []string{"Kenneth Goodwin"}...)
-
-	fmt.Println(players)
-	fmt.Println(typeof(players), len(players), cap(players))
-
-	headers := make(map[string]interface{})
-
-	headers["Content-Type"] = "application/json"
-	headers["Accept-Language"] = "zh-CH, en"
-	headers["Host"] = "localhost"
-
-	delete(headers, "Host")
-
-	fmt.Printf("%#v %t\n", headers, has(headers, "Host"))
-
-	pointerOfCities := &cities
-
-	pointerOfCities[3] = "BJ"
-	(*pointerOfCities)[5] = "SZ"
-
-	fmt.Println(cities, (pointerOfCities))
-
-	server := HTTPServer{}
-
-	server.Port = 8080
-
-	fmt.Printf("%#v %t\n", server, reflect.DeepEqual(server, HTTPServer{}))
-
-	post := object{
-		"id":    0,
-		"title": "",
-		"body":  "",
-	}
-
-	log(0, 0xff, post.toJson())
-
-	var label any = 0
-
-	label = ""
-
-	label = []string{}
-
-	fmt.Println(label)
 }
